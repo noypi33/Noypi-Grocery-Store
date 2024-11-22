33dbdf2adf315d119284d00df1d27596fb88bc31
noypi33 <novakvlatka09@gmail.com>
2024-11-22 17:54:31 +0100

    Add files via upload

---
+++ initialize.php
@@ -0,0 +1,10 @@
+<?php
+//$dev_data = array('id'=>'-1','firstname'=>'Joe','lastname'=>'Doe','username'=>'user','password'=>'0192023a7bbd73250516f069df18b500','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','http://tatakpinoy.si/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"localhost:3306");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"tatakpin_user");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"1b65wU)OZ3y@Tq");
+if(!defined('DB_NAME')) define('DB_NAME',"tatakpin_mvogms_db");
+?>
